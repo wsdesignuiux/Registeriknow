3433bffaf0415715cef468feeaea62818102ab7b
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-01 10:50:55 +0530

    Initial commit

---
+++ app/src/main/java/e/wolfsoft1/register_i_know/MainActivity3.java
@@ -0,0 +1,17 @@
+package e.wolfsoft1.register_i_know;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+    }
+}
